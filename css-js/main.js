35c04bba25bb65aab67948f6a26f486be401f343
Hadi <Al.Hadi@adpeople.com>
2015-05-13 23:33:50 +0600

    implementation of bitly added

--- css-js/main.js
+++ css-js/main.js
@@ -1,3 +1,46 @@
+function isUrl(s) {
+    var regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+    return regexp.test(s);
+}
+
 jQuery(document).ready(function($) {
-    console.log('gruntjs is now working :D');
+
+    $('.shorten-form .form').submit(function(event) {
+        event.preventDefault();
+
+        var $longUrl = $('input[name="longurl"]');
+        $longUrl.removeClass('is-error');
+        $(this).find('.form--error-text').addClass('is-hidden');
+
+        if($longUrl.val().length > 0 && isUrl($longUrl.val())) {
+            var longUrl = $longUrl.val();
+
+            $('.shorten-form--response').removeClass('is-hidden');
+            $('.shorten-form--response--url').val('Creating link ...');
+            $.ajax({
+                url: 'result.php',
+                type: 'POST',
+                dataType: 'json',
+                data: {long_url: longUrl}
+            })
+            .done(function(response) {
+                if(response.status_code == 200 && response.status_txt === 'OK') {
+                    $longUrl.attr('placeholder', $longUrl.val()).val('');
+                    $('.shorten-form--response--url').val(response.data.url).select().focus();
+                }
+                else {
+                    $('.shorten-form--response--url').val('Sorry, invalid URL!');
+                }
+            })
+            .fail(function() {
+                $('.shorten-form--response--url').val('Sorry, operation failed!');
+            });
+
+        }
+        else {
+            $longUrl.addClass('is-error');
+            $(this).find('.form--error-text').removeClass('is-hidden');
+        }
+    });
+
 });
--- result.php
+++ result.php
@@ -2,11 +2,21 @@
 header('content-type: application/json; charset=utf-8');
 header("access-control-allow-origin: *");
 
+$xhr = $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest';
+if(!$xhr) {
+    die();
+}
+
 require_once(__DIR__.'/settings.php');
 require_once(__DIR__.'/shorten.php');
 
-$long_path = 'http://habibhadi.com';
+$long_path = isset($_POST['long_url']) ? filter_var($_POST['long_url'], FILTER_SANITIZE_URL) : die();
 
-$shorten = new Bitly;
-$shorten->url = $long_path;
-echo $shorten->get_short_url();
+if (filter_var($long_path, FILTER_VALIDATE_URL)) {
+    $shorten = new Bitly;
+    $shorten->url = $long_path;
+    echo $shorten->get_short_url();
+}
+else {
+    die();
+}
